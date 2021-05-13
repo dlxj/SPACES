17ef4ccc47dfb27160b59408b21da28cf3ba0a80
dlxj <123435@qq.com>
2021-05-13 14:05:24 +0800

    Update autoClick.py

--- autoClick.py
+++ autoClick.py
@@ -1,6 +1,3 @@
-
-# 防止Colab 断线
-
 # windows
 # pip install pygetwindow==0.0.1
 # pip install pyautogui===0.9.0
@@ -11,9 +8,12 @@ import time
 print( pygui.position() )
 
 while True:
-    pygui.moveTo(555,555,10)
+    pygui.moveTo(555,555,15)
     # time.sleep(10)
-    pygui.moveTo(333,333,10)
+    pygui.moveTo(333,333,15)
     # time.sleep(10)
-    pygui.moveTo(666,666,10)
+    pygui.moveTo(666,666,15)
     # time.sleep(10)
+
+    pygui.moveTo(1711,184,15) # win10 colab busy button
+    pygui.click(x=1711, y=184, clicks=2, interval=10, button='left') # # win10 colab busy button

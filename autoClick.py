c2070d9c4672a62ee829a28f9ca51a6fd662637b
dlxj <123435@qq.com>
2021-05-19 09:47:36 +0800

    m

--- autoClick.py
+++ autoClick.py
@@ -17,3 +17,28 @@ while True:
 
     pygui.moveTo(1711,184,15) # win10 colab busy button
     pygui.click(x=1711, y=184, clicks=2, interval=10, button='left') # # win10 colab busy button
+
+
+"""
+for chrome - f12
+
+# start()开始
+# stop()结束
+
+function getElementByXpath(path) {
+       return document.evaluate(path, document, null, 
+       XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+ 
+function reconnect(){
+	  console.log('working')
+	  getElementByXpath("//div[@id='top-toolbar']/colab-connect-button").click()
+}
+var a = setInterval(reconnect, 1*60*1000);
+function stop(){
+	 clearInterval(a)
+}
+function start(){
+	 a = setInterval(reconnect, 1*60*1000);
+}
+"""
